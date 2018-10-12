68a0b114bc439c01768cb865cb7ebdc9b5974ce4
Caitlin Terzieff <cterzieff@feature23.com>
2018-10-12 14:16:43 -0400

    - initial creation

---
+++ DefectTracker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DefectTracker.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
